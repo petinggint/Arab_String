de44c7297cabc960666fd724507c5ce638e85d7e
petinggint <155607304+petinggint@users.noreply.github.com>
2024-01-16 15:40:50 +0700

    E callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -31,28 +31,28 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
-    elif query == "help":
+    elif query == "ʙᴀɴᴛᴜᴀɴ":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text="**Help related on how to use me...**\n" + Data.HELP,
+            text="**ʙᴀɴᴛᴜᴀɴ ᴛᴇʀᴋᴀɪᴛ ᴛᴇɴᴛᴀɴɢ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ꜱᴀʏᴀ...**\n" + Data.HELP,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
-    elif query == "generate":
+    elif query == "ᴍᴇɴɢᴀᴍʙɪʟ":
         await callback_query.message.reply(
             "Please choose the python library you want to generate string session for",
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
-                InlineKeyboardButton("Telethon", callback_data="telethon")
+                InlineKeyboardButton("ᴘʏʀᴏɢʀᴀᴍ", callback_data="pyrogram"),
+                InlineKeyboardButton("ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")
             ]])
         )
-    elif query in ["pyrogram", "telethon"]:
+    elif query in ["ᴘʏʀᴏɢʀᴀᴍ", "ᴛᴇʟᴇᴛʜᴏɴ"]:
         await callback_query.answer()
         try:
-            if query == "pyrogram":
+            if query == "ᴘʏʀᴏɢʀᴀᴍ":
                 await generate_session(bot, callback_query.message)
             else:
                 await generate_session(bot, callback_query.message, telethon=True)
