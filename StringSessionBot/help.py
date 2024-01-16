a8a4a2a6f1d02d2100c63d11d91cefac74a7054f
petinggint <155607304+petinggint@users.noreply.github.com>
2024-01-16 15:05:08 +0700

    E help.py

--- StringSessionBot/help.py
+++ StringSessionBot/help.py
@@ -8,6 +8,6 @@ from pyrogram.types import InlineKeyboardMarkup
 async def _help(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        "**Help related on how to use me...**\n" + Data.HELP,
+        "**ʙᴀɴᴛᴜᴀɴ ᴛᴇʀᴋᴀɪᴛ ᴛᴇɴᴛᴀɴɢ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ꜱᴀʏᴀ...**\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
